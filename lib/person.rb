e6c1ba30669ef3a7d6391e04350950538fc001be
Emily Morgan <emorgan05@gmail.com>
2017-09-04 15:42:07 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,4 +6,23 @@ class Person
   def walk
     puts "The Person is walking"
   end
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def name=(full_name)
+    @this_full_name = @first_name + @last_name
+  end
+
+  def name
+    @this_full_name
+  end
 end
+
+kanye = Person.new
+kanye.name = "Kanye"
+puts kanye.name
+kanye.name = "Yeezy"
+puts kanye.name
