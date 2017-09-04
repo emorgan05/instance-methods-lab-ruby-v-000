71edce82bde88069c40949a3ded721ac0c29cab5
Emily Morgan <emorgan05@gmail.com>
2017-09-04 15:47:36 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -13,11 +13,13 @@ class Person
   end
 
   def name=(full_name)
-    @this_full_name = @first_name + " " + @last_name
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
   end
 
   def name
-    @this_full_name
+    "#{@first_name} #{@last_name}".strip
   end
 end
 
